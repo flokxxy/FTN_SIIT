47bfa1733bd13bdd06f03ded7c9bf1421fd7028d
flokxxy <115942207+flokxxy@users.noreply.github.com>
2024-10-28 15:37:36 +0100

    Update main.cpp

--- OOP_2/vezba_4/zadatok_2/main.cpp
+++ OOP_2/vezba_4/zadatok_2/main.cpp
@@ -1,1 +1,27 @@
-//
+#include <iostream>
+#include <complex>
+#include "myComplex.h"
+
+
+using namespace std;
+
+
+int main(int argc, char** argv) {
+    Complex a(3.0, 4.0);
+    Complex b;
+    cout << "Enter a complex number b:" << endl;
+    cin >> b;
+    cout << "a (" << a.getReal() << ", " << a.getImag() << "i) " << endl;
+    cout << "b " << b << endl;
+    cout << "Complex conjugate b is " << ~b <<endl;
+    cout << "a + b is " << a + b << endl;
+    cout << a - b << endl;
+    cout << "a * b is " << a * b << endl;
+
+//cout << "2 * b is " << b * 2 << endl;
+//cout << "b * 2 is " << 2 * b << endl;
+
+    cout << "Done!" << endl;
+    return 0;
+
+}
