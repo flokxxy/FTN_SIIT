0b0c1aa90a10320a07e1390fe8e69e68462a9e98
flokxxy <115942207+flokxxy@users.noreply.github.com>
2024-10-28 14:05:06 +0100

    Create myComplex.h

---
+++ OOP_2/vezba_4/zadatok_1/myComplex.h
@@ -0,0 +1,46 @@
+//
+// Created by Виктория Аванесова on 28.10.2024.
+//
+
+#ifndef OOP2_VEZV_4_MYCOMPLEX_H
+#define OOP2_VEZV_4_MYCOMPLEX_H
+
+#include <iostream>
+
+using namespace std;
+
+
+class Complex{
+private:
+    double x;
+    double y;
+
+public:
+
+    Complex();
+    Complex(double real,double imag);
+    Complex(const Complex& other);
+
+    void setReal(double real);
+    void setImag(double imag);
+    double getReal();
+    double getImag();
+
+    Complex add(Complex num);
+    Complex sub(Complex num);
+    Complex mul(Complex num);
+    Complex conj(Complex num);
+
+    Complex operator+(Complex num);
+    Complex operator-(Complex num);
+    Complex operator*(Complex num);
+    Complex operator~();
+
+   /* friend istream& operator>>(istream& in, Complex& num);
+    friend ostream& operator<<(ostream& os, Complex& num);
+*/
+    string to_string();
+};
+
+
+#endif //OOP2_VEZV_4_MYCOMPLEX_H
