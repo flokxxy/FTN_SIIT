81f7ff074c84bec9bd75865abd59dc8e2d26abb2
flokxxy <115942207+flokxxy@users.noreply.github.com>
2024-11-26 15:56:13 +0100

    Update task1_my_NutonRapson.py

--- NPEA/pr_to_kol/task1_my_NutonRapson.py
+++ NPEA/pr_to_kol/task1_my_NutonRapson.py
@@ -45,3 +45,48 @@ def metod_NR(x,epsilon):
 
 print(metod_NR(x=-1,epsilon=0.01))
 
+
+///////////////////////////////////
+
+
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+#f(x)=e^x−3x^2
+
+def f(x):
+    return np.exp(x)-3*x**2
+def f_pr1(x):
+    return math.exp(x)-6*x
+
+def metod_NR(x,eps):
+    x1=x
+    x=math.inf
+    while abs(x1-x)>eps:
+        x=x1
+        x1 = x - f(x)/f_pr1(x)
+    return x1
+
+print(metod_NR(x=0.5,eps = 1e-6))
+print(metod_NR(x=0.3,eps = 1e-6))
+
+
+
+#проверка корней -->
+
+#диапазон значений х
+x = np.linspace(-1, 2, 500)
+y = np.exp(x)-3*x**2
+
+
+#построение графика
+plt.axhline(0, color='black', linewidth=0.8, linestyle='--')  # Ось X
+plt.plot(x, y, label=f(x))
+plt.title("График функции")
+plt.xlabel("x")
+plt.ylabel("f(x)")
+#plt.legend()
+plt.grid()
+plt.show()
+
